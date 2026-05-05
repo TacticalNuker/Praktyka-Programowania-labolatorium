850d7b16a097b2804fd439c205171355df9bb323
Kacper Ulanowski <ulanowski.kacper@gmail.com>
2026-05-05 18:18:53 +0200

    lab_5 krok 4.9

--- utils.py
+++ utils.py
@@ -1,13 +1,22 @@
 # Exemplary calculator functions
 
-def add (a: int , b: int) -> int:
+"""Moduł kalkulatora"""
+
+def add(a: int, b: int) -> int:
+    """Dodawanie"""
     return a + b
 
-def subtract (a: int , b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """Odejmowanie"""
     return a - b
 
-def multiply (a: int , b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """Mnożenie"""
     return a * b
 
-def divide (a: int , b: int) -> int:
+
+def divide(a: int, b: int) -> int:
+    """Dzielenie"""
     return a / b
